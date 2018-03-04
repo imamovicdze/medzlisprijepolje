daba5e302a13e50698b6a42a995b8bf2d8e365cb
imamovicdze <imamovicdze@gmail.com>
2018-03-04 18:29:26 +0100

    fixed template

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ use MedzlisPrijepolje\Controllers\DashboardController;
 $app = new Application($_SERVER['HOME']);
 
 $mainController = new MainController($app['mainService'],$app['twig']);
-$dashboardController = new DashboardController($app['dashboardService']);
+$dashboardController = new DashboardController($app['dashboardService'],$app['twig']);
+
 $app->get('/', [$mainController, "index"]);
 $app->get('/index', [$mainController, "index"]);
 $app->get('/medzlis', [$mainController, "medzlis"]);
@@ -25,10 +26,20 @@ $app->get('/contact', [$mainController, "contact"]);
 $app->get('/dashboard', [$mainController, "dashboard"]);
 
 
+// category CRUD
+$app->post('/category/create',[$dashboardController, "createCat"]);
+$app->get('/categories',[$dashboardController, "readCat"]);
+$app->post('/category/update/{id}',[$dashboardController, "updateCat"]);
+$app->post('/category/delete/{id}', [$dashboardController, "deleteCat"]);
 
-$app->post('/category/create',[$dashboardController, "create"]);
-$app->get('/categories',[$dashboardController, "get"]);
-$app->post('/category/update/{id}',[$dashboardController, "update"]);
+
+//News CRUD
+$app->post('/news/create', [$dashboardController, "createN"]);
+$app->get('/news',[$dashboardController, "readN"]);
+$app->post('/news/update/{id}',[$dashboardController, "updateN"]);
+$app->post('/news/delete/{id}', [$dashboardController, "deleteN"]);
+
+
 
 
 $app->run();
